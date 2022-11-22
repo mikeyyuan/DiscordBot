f579c196d4c3ae88ea01a8e4d465ba11883f124e
mikeyyuan <michaelyuan@nyu.edu>
2022-11-21 18:52:29 -0800

    first commit

---
+++ commands/avatar.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('avatar')
+		.setDescription('Report a user')
+        .addUserOption(option =>
+            option
+                .setName('target')
+                .setDescription('The member to report')
+                .setRequired(true))
+        .addStringOption(option =>
+            option
+                .setName('reason')
+                .setDescription('Report reason')
+                .setRequired(true)),
+    async execute(message, args) {
+       let member = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.member;
+       let avatar = member.user.displayAvatarURL({ size: 1024, dynamic: true });
+
+       const embed = new Discord.MessageEmbed()
+        .setTitle(`${member.user.username}'s Avatar`)
+        .setImage(avatar)
+        .setColor("BLACK")
+        .setAuthor(member.user.username);
+        message.channel.send({embeds: [embed]});
+    },
+};
